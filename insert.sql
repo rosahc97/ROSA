9cf4f7a5fa89cb4fef0d1eabfb93198f2242b7b3
Judith Rosado <jroslopz@gmail.com>
2019-10-14 10:27:48 +0200

    insertar aula

--- insert.sql
+++ insert.sql
@@ -3,3 +3,6 @@ VALUES("Manuel", "Gonzalez", 24, "Marchena");
 
 INSERT INTO curso (grado, nombre_curso)
 VALUES(2, "Administración de sistemas informáticos en red");
+
+INSERT INTO aula (nombre_aula, capacidad)
+VALUES("ASIR", 20);
