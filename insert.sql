1f326badbfa03b9a154a08bef528b76adb825cd8
Rebeca Suárez <rsbascon1998@gmail.com>
2019-10-14 10:16:53 +0200

    Insertar valores a tabla curso

--- insert.sql
+++ insert.sql
@@ -1,2 +1,5 @@
 INSERT INTO usuario (nombre, apellidos, edad, direccion)
-VALUES(Manuel, Gonzalez, 24, Marchena);
+VALUES("Manuel", "Gonzalez", 24, "Marchena");
+
+INSERT INTO curso (grado, nombre_curso)
+VALUES(2, "Administración de sistemas informáticos en red");
